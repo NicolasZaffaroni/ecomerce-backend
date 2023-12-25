e3e0f6d3d18e506d97aeb88016b51e568a7076c9
Nicolas Zaffaroni <117034184+NicolasZaffaroni@users.noreply.github.com>
2023-12-25 19:44:36 +0100

    Sumado handlbars y una plantilla de ejemplo

---
+++ src/configs/app.js
@@ -0,0 +1,13 @@
+import app from './server.config.js';
+
+
+
+
+const DEFAULT_PORT = 8085;
+const port = process.env.PORT || DEFAULT_PORT;
+
+
+//Servidor Local 
+app.listen(port, () => {
+    console.log("Servidor iniciado en: http://localhost:" + port);
+});
---
+++ src/configs/server.config.js
@@ -0,0 +1,25 @@
+
+import express from "express"
+import router from "/Users/nicozaffaroni95/Desktop/Proyectos/Ecommerce-Backend/src/routes/index.js"
+import handlebars   from "express-handlebars";
+
+
+const app = express()
+
+app.engine('handlebars',handlebars.engine())
+app.set('views',process.cwd() + '/src/views')
+
+app.get('/users',(req,res) => {
+    
+    res.render('users.handlebars')
+
+})
+//Encontrar ruta 
+// console.log(process.cwd())
+
+app.use(express.json())
+app.use(express.static('/Users/nicozaffaroni95/Desktop/Proyectos/Ecommerce-Backend'+'/src/public'))
+
+router(app)
+
+export default app
