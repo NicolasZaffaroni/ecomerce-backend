0590d4a2f9033f45245dc57da3da681a60f293d4
Nicolas Zaffaroni <117034184+NicolasZaffaroni@users.noreply.github.com>
2024-01-03 23:17:41 +0100

    agregue socket falta corregir rutas

--- src/configs/app.js
+++ src/configs/app.js
@@ -4,17 +4,29 @@ import { Server } from 'socket.io';
 
 
 
-const DEFAULT_PORT = 8088;
+const DEFAULT_PORT = 8080;
 const port = process.env.PORT || DEFAULT_PORT;
 
 
 //Servidor Local 
 const httpServer = app.listen(port, () => {
-    console.log("Servidor iniciado en: http://localhost:" + port);
+    console.log("Server running at port: http://localhost:" + port);
 });
 
 const io = new Server(httpServer)
 
 io.on('connection',socket => {
     console.log(socket.id)
+
+    socket.on('message', data =>{
+        console.log(data)
+    })
+
+    socket.emit('messageServer', 'hi stalker !!')
+
+    socket.broadcast.emit('messageOthers','hola a todos menos al principal ')
+
+    io.emit('messageAll','hola a todos inlcuido el principal')
 })
+
+
--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,2 +1,16 @@
 
 const socket = io()
+
+socket.emit("message",'hello stalkers')
+
+socket.on('messageServer', data =>{
+    console.log(data)
+})
+
+socket.on('messageOthers', data =>{
+    console.log(data)
+})
+
+socket.on('messageAll', data =>{
+    console.log(data)
+})
